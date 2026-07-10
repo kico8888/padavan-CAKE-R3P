8e28db4744775405bd79d525ccbd4a0694863d2d
kico8888 <71839416+kico8888@users.noreply.github.com>
2026-07-10 16:35:11 +0800

    Update menu.js

--- trunk/user/www/locale/zh-TW/menu.js
+++ trunk/user/www/locale/zh-TW/menu.js
@@ -1,1 +1,32 @@
+var menu = {
+
+  "status": "狀態資訊",
+  "system": "系統",
+  "network": "網路",
+  "wireless": "無線網路",
+  "firewall": "防火牆",
+  "usb": "USB 功能",
+  "vpn": "VPN",
+  "qos": "QoS",
+  "advanced": "進階設定",
+  "administration": "管理",
+  "logout": "登出",
+
+  // 子選單
+  "sys_info": "系統資訊",
+  "sys_log": "系統日誌",
+  "wan": "WAN 設定",
+  "lan": "LAN 設定",
+  "wifi_24": "2.4GHz Wi‑Fi",
+  "wifi_5": "5GHz Wi‑Fi",
+  "dhcp": "DHCP 伺服器",
+  "dns": "DNS 設定",
+  "port_forward": "連接埠轉發",
+  "mac_filter": "MAC 過濾",
+  "dmz": "DMZ 主機",
+  "reboot": "重新啟動",
+  "backup": "備份 / 還原",
+  "upgrade": "韌體升級",
+  "time": "時間設定"
+};
 
