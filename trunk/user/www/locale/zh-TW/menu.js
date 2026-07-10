7dea2438bf50788d08a91c72fb6536ab234b19cd
kico8888 <71839416+kico8888@users.noreply.github.com>
2026-07-10 16:40:26 +0800

    Update menu.js

--- trunk/user/www/locale/zh-TW/menu.js
+++ trunk/user/www/locale/zh-TW/menu.js
@@ -1,5 +1,6 @@
 var menu = {
 
+  // 主選單
   "status": "狀態資訊",
   "system": "系統",
   "network": "網路",
@@ -7,26 +8,62 @@ var menu = {
   "firewall": "防火牆",
   "usb": "USB 功能",
   "vpn": "VPN",
-  "qos": "QoS",
+  "qos": "QoS 流量管理",
   "advanced": "進階設定",
   "administration": "管理",
   "logout": "登出",
 
-  // 子選單
+  // 系統
   "sys_info": "系統資訊",
   "sys_log": "系統日誌",
+  "time": "時間設定",
+  "upgrade": "韌體升級",
+  "backup": "備份 / 還原",
+  "reboot": "重新啟動",
+
+  // 網路
   "wan": "WAN 設定",
   "lan": "LAN 設定",
-  "wifi_24": "2.4GHz Wi‑Fi",
-  "wifi_5": "5GHz Wi‑Fi",
   "dhcp": "DHCP 伺服器",
   "dns": "DNS 設定",
+  "route": "路由設定",
+
+  // 無線
+  "wifi_24": "2.4GHz Wi‑Fi",
+  "wifi_5": "5GHz Wi‑Fi",
+  "wifi_wps": "WPS 設定",
+  "wifi_mac": "無線 MAC 過濾",
+
+  // 防火牆
   "port_forward": "連接埠轉發",
   "mac_filter": "MAC 過濾",
   "dmz": "DMZ 主機",
-  "reboot": "重新啟動",
-  "backup": "備份 / 還原",
-  "upgrade": "韌體升級",
-  "time": "時間設定"
-};
+  "security": "安全性設定",
 
+  // USB
+  "usb_storage": "USB 儲存裝置",
+  "usb_samba": "Samba 分享",
+  "usb_ftp": "FTP 伺服器",
+  "usb_printer": "印表機伺服器",
+
+  // VPN
+  "vpn_pptp": "PPTP VPN",
+  "vpn_l2tp": "L2TP VPN",
+  "vpn_ovpn": "OpenVPN 客戶端",
+
+  // QoS
+  "qos_rules": "流量規則",
+  "qos_devices": "裝置優先權",
+
+  // 進階設定
+  "nat": "NAT 設定",
+  "ipv6": "IPv6 設定",
+  "vlan": "VLAN 設定",
+  "mtu": "MTU 設定",
+
+  // 診斷工具
+  "diag": "診斷工具",
+  "diag_ping": "Ping 測試",
+  "diag_trace": "路由追蹤",
+  "diag_arp": "ARP 表"
+};
