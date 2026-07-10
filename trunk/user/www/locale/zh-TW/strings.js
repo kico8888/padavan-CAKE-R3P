683735941c44ddff2a6697a8c9f56c7ecaf33f91
kico8888 <71839416+kico8888@users.noreply.github.com>
2026-07-10 16:34:43 +0800

    Update strings.js

--- trunk/user/www/locale/zh-TW/strings.js
+++ trunk/user/www/locale/zh-TW/strings.js
@@ -1,1 +1,64 @@
+var lang = {
+
+  // 基本介面
+  "apply": "套用",
+  "save": "儲存設定",
+  "cancel": "取消",
+  "ok": "確定",
+  "close": "關閉",
+  "enable": "啟用",
+  "disable": "停用",
+  "delete": "刪除",
+  "edit": "編輯",
+  "refresh": "重新整理",
+  "loading": "載入中…",
+
+  // 系統資訊
+  "system_info": "系統資訊",
+  "firmware_version": "韌體版本",
+  "kernel_version": "核心版本",
+  "uptime": "運作時間",
+  "cpu_load": "CPU 使用率",
+  "memory_usage": "記憶體使用量",
+
+  // 網路設定
+  "network_settings": "網路設定",
+  "wan_settings": "WAN 設定",
+  "lan_settings": "LAN 設定",
+  "wifi_settings": "Wi‑Fi 設定",
+  "wifi_24g": "2.4GHz 無線網路",
+  "wifi_5g": "5GHz 無線網路",
+  "ssid": "SSID 名稱",
+  "password": "密碼",
+  "channel": "頻道",
+  "bandwidth": "頻寬",
+  "security": "安全性",
+
+  // DHCP / DNS
+  "dhcp_settings": "DHCP 設定",
+  "dns_settings": "DNS 設定",
+  "dhcp_client_list": "DHCP 客戶端列表",
+
+  // 防火牆
+  "firewall": "防火牆",
+  "port_forwarding": "連接埠轉發",
+  "dmz": "DMZ 主機",
+  "mac_filter": "MAC 過濾",
+
+  // 系統管理
+  "system_management": "系統管理",
+  "reboot": "重新啟動",
+  "restore": "還原設定",
+  "backup": "備份設定",
+  "upgrade": "韌體升級",
+
+  // 其他
+  "usb_settings": "USB 設定",
+  "vpn_settings": "VPN 設定",
+  "qos_settings": "QoS 流量管理",
+  "log_viewer": "系統日誌",
+  "time_settings": "時間設定",
+  "timezone": "時區",
+  "ntp_server": "NTP 時間伺服器"
+};
 
